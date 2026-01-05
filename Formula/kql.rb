166723c8735b67f8a5f08f4f5bfa6e9ac4b2a962
cloudygreybeard <phijones@redhat.com>
2026-01-05 17:57:42 +1100

    Restore kql formula v0.2.0

---
+++ Formula/kql.rb
@@ -0,0 +1,38 @@
+class Kql < Formula
+  desc "CLI toolkit for Kusto Query Language (KQL) and Azure Data Explorer"
+  homepage "https://github.com/cloudygreybeard/kql"
+  version "0.2.0"
+  license "Apache-2.0"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/cloudygreybeard/kql/releases/download/v#{version}/kql_#{version}_darwin_amd64.tar.gz"
+      sha256 "315e371134c46630089431e233d5a79232f8a2f8be0f5141e3e32be4a5813d67"
+    end
+    on_arm do
+      url "https://github.com/cloudygreybeard/kql/releases/download/v#{version}/kql_#{version}_darwin_arm64.tar.gz"
+      sha256 "f395544d0584f6f8fc1da9aa3854d4cea008ea3e89bbf9e54cb5b254affc871c"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/cloudygreybeard/kql/releases/download/v#{version}/kql_#{version}_linux_amd64.tar.gz"
+      sha256 "4cf94e8785f2f72f789579aca5fef0817570ddcd8451ba1dc6c558006e55df44"
+    end
+    on_arm do
+      url "https://github.com/cloudygreybeard/kql/releases/download/v#{version}/kql_#{version}_linux_arm64.tar.gz"
+      sha256 "2338c237774c75acaea4904095accdd29c13ca2ce470fe9bd1430b34d79a4855"
+    end
+  end
+
+  def install
+    bin.install "kql"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/kql version")
+    output = shell_output("#{bin}/kql link build 2>&1", 1)
+    assert_match "required flag", output
+  end
+end
